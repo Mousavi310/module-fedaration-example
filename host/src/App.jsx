be8177b937d91cdd45875ffd1c2513b7440e8ef9
Morteza <mousavi310@gmail.com>
2024-12-28 20:59:16 +0100

    Add simple header.

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -6,6 +6,7 @@ const Products = lazy(() => import('App1/Products'));
 export default function App() {
     return (
         <div className="main">
+            <h1>Host application</h1>
             <Suspense fallback={<div>Loading Header...</div>}>
                 <Products />
             </Suspense>
